1142f52ddb36cd5aee59d452137aeb6403397948
shea1108 <shea11082003@gmail.com>
2024-09-09 09:50:58 +0700

    Serve static files

--- src/app.js
+++ src/app.js
@@ -13,10 +13,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'))
+
 
 app.get('/', (req, res) => {
   return res.json(JSend.success());
 });
+
+
 
 contactsRouter.setup(app);
 
