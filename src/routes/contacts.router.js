585cd7ee8a4a1e7a7fb7ebbd2cd598be19e9c929
shea1108 <shea11082003@gmail.com>
2024-09-09 09:28:41 +0700

    Standardize JSON responese

---
+++ src/routes/contacts.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const contactsController = require('../controllers/contacts.controller');
+const { methodNotAllowed } = require('../controllers/errors.controller');
+
+const router = express.Router();
+
+module.exports.setup = (app) => {
+  app.use('/api/v1/contacts', router);
+
+  router.get('/', contactsController.getContactsByFilter);
+  router.post('/', contactsController.createContact);
+  router.delete('/', contactsController.deleteAllContacts);
+  router.all('/', methodNotAllowed);
+
+  router.get('/:id', contactsController.getContact);
+  router.put('/:id', contactsController.updateContact);
+  router.delete('/:id', contactsController.deleteContact);
+  router.all('/:id', methodNotAllowed);
+};
