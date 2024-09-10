8c68cd946f1b3b400667baa6fee9853c9a6d47b4
shea1108 <shea11082003@gmail.com>
2024-09-10 15:42:49 +0700

    Generate OpenAPI Spec from JSDoc

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,41 +1,51 @@
 const JSend = require('../jsend');
+
 function createContact(req, res) {
-return res.status(201).json(JSend.success({ contact: {} }));
-}
-function getContactsByFilter(req, res) {
-const filters = [];
-const { favorite, name } = req.query;
-if (favorite !== undefined) {
-filters.push(`favorite=${favorite}`);
+  return res.status(201).json(JSend.success({ contact: {} }));
 }
 
-if (name) {
-filters.push(`name=${name}`);
+function getContactsByFilter(req, res) {
+  const filters = [];
+  const { favorite, name } = req.query;
+
+  if (favorite !== undefined) {
+    filters.push(`favorite=${favorite}`);
+  }
+
+  if (name) {
+    filters.push(`name=${name}`);
+  }
+
+  console.log(filters.join('&'));
+
+  return res.json(
+    JSend.success({
+      contacts: [],
+    })
+  );
 }
-console.log(filters.join('&'));
-return res.json(
-JSend.success({
-contacts: [],
-})
-);
-}
+
 function getContact(req, res) {
-return res.json(JSend.success({ contact: {} }));
+  return res.json(JSend.success({ contact: {} }));
 }
+
 function updateContact(req, res) {
-return res.json(JSend.success({ contact: {} }));
+  return res.json(JSend.success({ contact: {} }));
 }
+
 function deleteContact(req, res) {
-return res.json(JSend.success());
+  return res.json(JSend.success());
 }
+
 function deleteAllContacts(req, res) {
-return res.json(JSend.success());
+  return res.json(JSend.success());
 }
+
 module.exports = {
-getContactsByFilter,
-deleteAllContacts,
-getContact,
-createContact,
-updateContact,
-deleteContact,
+  getContactsByFilter,
+  deleteAllContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
 };
